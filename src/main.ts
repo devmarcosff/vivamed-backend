9b47244efeff0ef696d67804964657c49136724e
unknown <devmarcosff@gmail.com>
2024-10-02 01:19:41 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL, 'https://vivamed.stevanini.com.br', 'https://vivamedbji.vercel.app'], // Adicione outras origens se necessário
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(8000);
 }
 bootstrap();
