1aa85643434f3a08c9eefccf5e14932e0e905cfb
unknown <devmarcosff@gmail.com>
2024-11-02 11:06:54 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    origin: [process.env.FRONTEND_URL, 'https://fcff-168-195-162-240.ngrok-free.app', 'http://localhost:3000', 'https://vivamed.stevanini.com.br', 'https://vivamedbji.vercel.app'],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:3000', 'https://vivamed.stevanini.com.br', 'https://vivamedbji.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
