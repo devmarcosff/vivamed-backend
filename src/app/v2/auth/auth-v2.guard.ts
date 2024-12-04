e6c44966d3a7742f4d88937ac7e64b82355e30c5
Romulo Fernandes Gouvêa <romulo-gouvea@hotmail.com>
2024-12-04 00:38:12 -0300

    fix: Bloqueando todas as rotas quando o usuário não está logado

--- src/app/v2/auth/auth-v2.guard.ts
+++ src/app/v2/auth/auth-v2.guard.ts
@@ -2,20 +2,43 @@ import {
     CanActivate,
     ExecutionContext,
     Injectable,
+    SetMetadata,
     UnauthorizedException
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { InjectDataSource } from '@nestjs/typeorm';
 import { Request } from 'express';
+import { DataSource } from 'typeorm';
+import { UserV2 } from '../user/entities/user-v2.entity';
 import { VivamedJwtService } from '../vivamed-jwt-module/jwt.service';
 import { UserV2JwtPayload } from './interfaces/user-v2-jwt.interface';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 @Injectable()
 export class AuthV2Guard implements CanActivate {
 
     private readonly accessTokenSecret = process.env.JWT_SECRET_KEY;
 
-    constructor(private vivamedJwtService: VivamedJwtService) { }
+    constructor(
+        private readonly vivamedJwtService: VivamedJwtService,
+        private readonly reflector: Reflector,
+        @InjectDataSource()
+        private readonly dataSource: DataSource,
+    ) { }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
+
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+
+        if (isPublic) {
+            return true;
+        }
+
         const request = context.switchToHttp().getRequest();
         const token = this.extractTokenFromHeader(request);
 
@@ -31,10 +54,25 @@ export class AuthV2Guard implements CanActivate {
             }
             const payload = await this.vivamedJwtService.decode<UserV2JwtPayload>(token);
 
+            await this.dataSource.transaction(async (manager) => {
+                const userRepository = manager.getRepository(UserV2);
+                const user = await userRepository.findOneBy({ id: payload.sub, enabled: true });
+                if (!user) {
+                    throw new UnauthorizedException('Usuário não encontrado');
+                }
+
+                if (!user.accessToken) {
+                    throw new UnauthorizedException('Usuário não está logado, faça o login novamente');
+                }
+            });
+
             request['user'] = payload;
 
             return true;
         } catch (error) {
+            if (error.name === 'TokenExpiredError') {
+                throw new UnauthorizedException('Token expirado');
+            }
             throw error;
         }
     }
